d3673142525f3dd14fff65819b6a97e8ad1064de
Robert Pratl <34143233+RobbyPratl@users.noreply.github.com>
2022-01-25 16:34:08 +0000

    added functionality

---
+++ config_generator.js
@@ -0,0 +1,16 @@
+/*
+will be used in initial setup of discord bot to generate 
+a .env file. KEEP THIS A SECRET DONT SHARE WITH OTHERS
+*/
+fs = require('fs');
+function generateEnvString(API_KEY, PRIV_KEY){
+  var api_key_prefix = "API_KEY=";
+  var priv_key_prefix = "PRIV_KEY="
+  var process = api_key_prefix + API_KEY+ "\n" +priv_key_prefix + PRIV_KEY;
+
+  fs.writeFile('./process.env',process, function (err) {
+  if (err) return console.log(err);
+});
+}
+
+generateEnvString("APIKEYTEST","PRIV_KEY")
---
+++ json_generator.js
@@ -0,0 +1,49 @@
+function convertJsonToBase64(data){
+    let buff = Buffer.from(data);
+    let base64data = buff.toString('base64');
+    return 'data:application/json;base64,'+base64data
+}
+
+function convertImageToBase64(contents,type = null){
+    var returnedData;
+    if (type == "svg"){
+        let imageBuff = Buffer.from(contents);
+        let base64ImageData = imageBuff.toString('base64');
+        returnedData = 'data:image/svg+xml;base64,' + base64ImageData;
+    }else if(type == "link"){
+        returnedData = contents;
+    }else if(type == null){
+      console.log("No type passed -- program assumes you passed in link!")
+      returnedData = contents;
+    }else{
+        throw "Contents argument must be in form of ascii link or svg data!!";
+    }
+    return returnedData;
+}
+
+function generateJSON(name,desc,image){
+    var dict = {};
+    dict["name"] = name.toString();
+    dict["description"] = desc.toString();
+    dict["image"] = image.toString();
+    return JSON.stringify(dict);var fs = require('fs');
+}
+
+
+/*
+Generate Encoded JSON for an NFT by passing in three strings: name, description,image.
+Type is prefered, not required if using a link to image, but required for passing in an svg 
+*/
+function generateEncoded(name,desc,image,type = null) {
+  console.log("Generating JSON");
+  imageToBase64 = convertImageToBase64(image,type);
+
+  generatedJSON = generateJSON(name,desc,imageToBase64);
+  
+  return convertJsonToBase64(generatedJSON);
+
+}
+
+encoded = generateEncoded("nametest","desctest","imagetest", "link")
+
+console.log(encoded)
