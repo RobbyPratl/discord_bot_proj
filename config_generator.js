6950a2c30244a7f1cc925476278ce7684af8ce25
Robert Pratl <34143233+RobbyPratl@users.noreply.github.com>
2022-01-25 18:25:52 +0000

    added package encodingtools

--- config_generator.js
+++ config_generator.js
@@ -5,8 +5,8 @@ a .env file. KEEP THIS A SECRET DONT SHARE WITH OTHERS
 fs = require('fs');
 function generateEnvString(API_KEY, PRIV_KEY){
   var api_key_prefix = "API_KEY=";
-  var priv_key_prefix = "PRIV_KEY="
-  var process = api_key_prefix + API_KEY+ "\n" +priv_key_prefix + PRIV_KEY;
+  var priv_key_prefix = "PRIV_KEY=";
+  var process = api_key_prefix + API_KEY+ "\n"+priv_key_prefix + PRIV_KEY;
 
   fs.writeFile('./process.env',process, function (err) {
   if (err) return console.log(err);
